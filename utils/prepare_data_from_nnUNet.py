768a26aff6e043fb39bc28bff78fcfc4e979f581
Pei-Shan Wang <53029479+Pei-eng@users.noreply.github.com>
2024-10-12 21:53:44 +0800

    Update prepare_data_from_nnUNet.py

--- utils/prepare_data_from_nnUNet.py
+++ utils/prepare_data_from_nnUNet.py
@@ -50,23 +50,9 @@ def resample_nii(input_path: str, output_path: str, target_spacing: tuple = (1.5
     
     save_image.save(output_path)
 
-dataset_root = "./data"
+dataset_root = "/content/SAM-Med3D/data/iNPH/train"
 dataset_list = [
-'/content/SAM-Med3D/data/iNPH/train/NPH/NPH_L_Ventri',
-'/content/SAM-Med3D/data/iNPH/train/NPH/NPH_R_Ventri',
-'/content/SAM-Med3D/data/iNPH/train/NPH/NPH_T_Ventri',
-'/content/SAM-Med3D/data/iNPH/train/Normal/Nor_Adu_F_L',
-'/content/SAM-Med3D/data/iNPH/train/Normal/Nor_Adu_F_R',
-'/content/SAM-Med3D/data/iNPH/train/Normal/Nor_Adu_F_T',
-'/content/SAM-Med3D/data/iNPH/train/Normal/Nor_Adu_M_L',
-'/content/SAM-Med3D/data/iNPH/train/Normal/Nor_Adu_M_R',
-'/content/SAM-Med3D/data/iNPH/train/Normal/Nor_Adu_M_T',
-'/content/SAM-Med3D/data/iNPH/train/Normal/Nor_Eld_F_L',
-'/content/SAM-Med3D/data/iNPH/train/Normal/Nor_Eld_F_R',
-'/content/SAM-Med3D/data/iNPH/train/Normal/Nor_Eld_F_T',
-'/content/SAM-Med3D/data/iNPH/train/Normal/Nor_Eld_M_L',
-'/content/SAM-Med3D/data/iNPH/train/Normal/Nor_Eld_M_R',
-'/content/SAM-Med3D/data/iNPH/train/Normal/Nor_Eld_M_T',
+'NPH/NPH_L_Ventri',
 ] # 'AMOS_val'
 
 target_dir = "./data/medical_preprocessed"
